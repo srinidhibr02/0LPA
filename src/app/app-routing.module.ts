7e8b0c7a39d4574d9df43c87d77edb85f5bf9817
srinidhibr02 <srinidhibr02@gmail.com>
2022-11-16 11:46:34 +0530

    styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { AboutComponent } from './page/about/about.component';
+import { WelcomeComponent } from './page/welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'welcome'
+  },
+  {
+    path:'welcome',
+    component:WelcomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'**',
+    redirectTo:'error'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
