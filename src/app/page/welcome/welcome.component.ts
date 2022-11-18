c76dff4a61df865abcf8ec11a028dc06bf00185a
srinidhibr02 <srinidhibr02@gmail.com>
2022-11-18 21:16:03 +0530

    fin

--- src/app/page/welcome/welcome.component.ts
+++ src/app/page/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js' 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -10,6 +10,15 @@ export class WelcomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const options = {
+      strings: ['Innovate.', 'Grow.', 'Design.', 'Learn.', 'Build.', 'Create.'],
+      typeSpeed: 100,
+      backSpeed:100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+    }
+    const typed = new Typed('.changingText',options);
   }
 
 }
